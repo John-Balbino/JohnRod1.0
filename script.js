a92ff4712861618601f2bef1ebbc8422d1479d90
JohnBalbino <john.balbino01@gmail.com>
2026-04-01 21:59:36 -0300

    alteração script

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-const nEncode = 'MTE5NTk3NjgzMDE='
-const number = atob(nEncode)
-const link ='https://wa.me/' + number
 
 function whats(){
+    const nEncode = 'MTE5NTk3NjgzMDE='
+    const number = atob(nEncode)
+    const link ='https://wa.me/' + number
     window.open(link)
 }
 
@@ -11,31 +11,7 @@ function whats(){
 
 
 
-function orcamento(){
-    
-    const btn = document.querySelectorAll('.txt button')
-    
-    /*btn.addEventListener('click', orcamento)
-    
-    let texto = document.querySelectorAll('.txt p')[0].textContent
-    let value = document.querySelectorAll('.txt span')[0].textContent
-    window.alert(texto  + value)
-    
-    event.preventDefault(orcamento)*/
-    
-    btn.forEach((btn,index)=>{
-        btn.addEventListener('click', (event) =>{
-            event.preventDefault()
 
-            let texto = document.querySelectorAll('.txt p')[index].textContent
-            let value = document.querySelectorAll('.txt span')[index].textContent
-            const msg = `Olá gostaria de uma ${texto}, no valor de : ${value}`
-            window.open(`${link}?text=${encondeURIComponent(msg)}`)
-            
-        })
-    })
-}
-orcamento();
 
 
 
