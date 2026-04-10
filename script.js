3bbaf311cc9697489dd41e16174f42f8f236b01a
JohnBalbino <john.balbino01@gmail.com>
2026-04-10 10:19:10 -0300

    ajuste na seção de valores

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 
-
+//função para whatsapp
 function whats(){
     const nEncode = 'MTE5NTk3NjgzMDE='
     const number = atob(nEncode)
     const link ='https://wa.me/' + number
     window.open(link)
 }
+
+
+//script para os cards sobre
+
+$('.card-txt').hide();
+$('.card-txt2').hide();
+$('.card-txt3').hide();
+
+$('#btn-txt').on('click',function(){
+    $('.card-txt').fadeToggle(1000);
+    
+})
+
+$('#btn-txt2').on('click',function(){
+    $('.card-txt2').fadeToggle(1000);
+})
+$('#btn-txt3').on('click',function(){
+    $('.card-txt3').fadeToggle(1000);
+})
 
 
 
